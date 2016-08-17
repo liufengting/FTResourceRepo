465d25b17967067904e8203a6fc840e7ff35637b
liufengting <wo157121900@me.com>
2016-08-17 20:48:25 +0800

    edit demo

---
+++ FTIBInspectable/FTIBInspectable/FTPopoverViewController.swift
@@ -0,0 +1,52 @@
+//
+//  FTViewController.swift
+//  FTIBInspectable
+//
+//  Created by liufengting on 16/8/17.
+//  Copyright © 2016年 liufengting. All rights reserved.
+//
+
+import UIKit
+
+class FTPopoverViewController: UIViewController,UIPopoverPresentationControllerDelegate,UIAdaptivePresentationControllerDelegate {
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.setup()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setup()
+
+    }
+
+    func setup() {
+        self.modalPresentationStyle = .Popover
+        self.popoverPresentationController?.delegate = self
+        self.automaticallyAdjustsScrollViewInsets = true
+    }
+
+    func prepareForPopoverPresentation(popoverPresentationController: UIPopoverPresentationController) {
+        self.popoverPresentationController?.passthroughViews = nil
+        self.popoverPresentationController?.popoverLayoutMargins = UIEdgeInsetsMake(40, 0, 0, 0)
+        self.popoverPresentationController?.backgroundColor = self.view.backgroundColor
+        self.popoverPresentationController?.permittedArrowDirections = .Any//[.Up,.Down,.Any]
+    }
+    
+    func adaptivePresentationStyleForPresentationController(controller: UIPresentationController) -> UIModalPresentationStyle {
+        if UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiom.Pad {
+            return .FullScreen
+        }else{
+            return .None
+        }
+    }
+    
+    func presentationController(controller: UIPresentationController, viewControllerForAdaptivePresentationStyle style: UIModalPresentationStyle) -> UIViewController? {
+        let nav = UINavigationController.init(rootViewController: controller.presentedViewController)
+        
+        return nav
+    }
+
+}
--- FTIBInspectable/FTIBInspectable/FTView.swift
+++ FTIBInspectable/FTIBInspectable/FTView.swift
@@ -16,3 +16,49 @@ import UIKit
         }
     }
 }
+
+
+//
+//  UIViewController+Extensions.swift
+//  Photog
+//
+//  Created by Andrew Patton on 2015-01-17.
+//  Copyright (c) 2015 acusti.ca. All rights reserved.
+//
+
+import Foundation
+
+//extension UIViewController: UIPopoverPresentationControllerDelegate {
+//    
+//
+//    
+//    // Popover presentation helpers
+//    // ----------------------------
+//    func presentViewControllerAsPopover(viewController: UIViewController, anchor: UIView) {
+//        if let presentedVC = self.presentedViewController {
+//            if presentedVC.nibName == viewController.nibName {
+//                // The view is already being presented
+//                return
+//            }
+//        }
+//        // Specify presentation style first (makes the popoverPresentationController property available)
+//        viewController.modalPresentationStyle = .Popover
+//        let viewPresentationController = viewController.popoverPresentationController
+//        // Prep source rect
+//        let sourceRectWidth = anchor.frame.width < 100 ? anchor.frame.width : 100
+//        if let presentationController = viewPresentationController {
+//            presentationController.delegate                 = self
+//            presentationController.sourceView               = anchor
+//            presentationController.sourceRect               = CGRectMake(0, 0, sourceRectWidth, anchor.frame.size.height)
+//            presentationController.permittedArrowDirections = .Up
+//        }
+//        viewController.preferredContentSize = CGSize(width: 230, height: 110)
+//        
+//        self.presentViewController(viewController, animated: true, completion: nil)
+//    }
+//    
+//    // Delegate method to allow popovers to be presented in narrow horizontal contexts not fullscreen
+//    public func adaptivePresentationStyleForPresentationController(controller: UIPresentationController) -> UIModalPresentationStyle {
+//        return .None
+//    }
+//}
