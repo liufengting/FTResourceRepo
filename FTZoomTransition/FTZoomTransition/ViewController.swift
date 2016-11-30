123baf4e73e953832583747d6460e4b786164f66
liufengting <wo157121900@me.com>
2016-11-30 20:52:15 +0800

    add FTZoomTransition

--- FTZoomTransition/FTZoomTransition/ViewController.swift
+++ FTZoomTransition/FTZoomTransition/ViewController.swift
@@ -22,68 +22,23 @@ class ViewController: UIViewController {
 
 
     }
-    
-    
-    // ********************************************//
-    // change iWannaTryPresent to see present/push //
-    //*********************************************//
-    
-    private let iWannaTryPresent = false
-    
-    // ********************************************//
-    // change iWannaTryPresent to see present/push //
-    //*********************************************//
-    
-
 
     @IBAction func handleButtonTap(_ sender: UIButton) {
         
+        // present
         
-        if iWannaTryPresent {
-            
-            // present
-            
-            let detial = self.storyboard?.instantiateViewController(withIdentifier: "DetialNavigationController") as! UINavigationController
-            let detialVC = detial.viewControllers[0] as! DetialViewController
-            let element = FTZoomTransitionElement(sourceView: sender,
-                                                  sourceSnapView: sender.snapshotView(afterScreenUpdates: true),
-                                                  sourceFrame: sender.frame,
-                                                  targetFrame: detialVC.targetImageView.frame)
-            transitionDelegate.element = element
-            detial.modalPresentationStyle = .custom
-            detial.transitioningDelegate = transitionDelegate
-            self.present(detial, animated: true, completion: {})
-        }else{
-            
-             //try push !!! basically the same !!!
-            
-            let detial = self.storyboard?.instantiateViewController(withIdentifier: "DetialViewController") as! DetialViewController
-            let element = FTZoomTransitionElement(sourceView: sender,
-                                                  sourceSnapView: sender.snapshotView(afterScreenUpdates: true),
-                                                  sourceFrame: sender.frame,
-                                                  targetFrame: detial.targetImageView.frame)
-            transitionDelegate.element = element
-            detial.modalPresentationStyle = .custom
-            detial.transitioningDelegate = transitionDelegate
-            self.present(detial, animated: true, completion: {})
-            navigationController?.delegate = FTNavigationDelegate()
-            navigationController?.pushViewController(detial, animated: true)
-            
-        }
-        
-        
-        
+        let detial = self.storyboard?.instantiateViewController(withIdentifier: "DetialNavigationController") as! UINavigationController
+        let detialVC = detial.viewControllers[0] as! DetialViewController
+        let element = FTZoomTransitionElement(sourceView: sender,
+                                              sourceSnapView: sender.snapshotView(afterScreenUpdates: true),
+                                              sourceFrame: sender.frame,
+                                              targetFrame: detialVC.targetImageView.frame)
+        transitionDelegate.element = element
+        detial.modalPresentationStyle = .custom
+        detial.transitioningDelegate = transitionDelegate
+        self.present(detial, animated: true, completion: {})
 
-
-        
-        
-        
-        
-
-
-        
-        
     }
-    
+
 }
 
