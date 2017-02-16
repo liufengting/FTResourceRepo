14f5c91ce2e347ff83cd616bfe26074341413143
liufengting <wo157121900@me.com>
2017-02-16 10:25:36 +0800

    edit image

---
+++ FTTesting/FTTesting/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FTTesting
+//
+//  Created by liufengting on 2017/2/7.
+//  Copyright © 2017年 LiuFengting <https://github.com/liufengting>. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+    }
+
+
+}
+
